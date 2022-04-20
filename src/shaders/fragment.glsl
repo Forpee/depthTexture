046972f9477a47127caf1d36ddaf4197e737d224
Forpee <khalilhassam3@gmail.com>
2022-04-20 20:29:31 +0200

    update

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -6,6 +6,7 @@ uniform float cameraFar;
 uniform sampler2D depthInfo;
 
 varying vec2 vUv;
+varying vec2 vUv1;
 
 float readDepth(sampler2D depthSampler,vec2 coord){
     float fragCoordZ=texture2D(depthSampler,coord).x;
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -5,6 +5,10 @@ uniform float cameraFar;
 uniform sampler2D depthInfo;
 
 varying vec2 vUv;
+varying vec2 vUv1;
+
+attribute float y;
+
 float readDepth(sampler2D depthSampler,vec2 coord){
     float fragCoordZ=texture2D(depthSampler,coord).x;
     float viewZ=perspectiveDepthToViewZ(fragCoordZ,cameraNear,cameraFar);
@@ -14,9 +18,13 @@ void main()
 {
     // vec4 mvPosition=modelViewMatrix*vec4(position,1.);
     // gl_PointSize=100.*(1./-mvPosition.z);
-    float depth=readDepth(depthInfo,uv);
+    vec2 vUv1 = vec2(uv.x, y);
+    
+    float depth=readDepth(depthInfo,vUv1);
     vec3 pos = position;
-    pos.z+=(1.-depth)*0.6;
+
+
+    pos.z+=(1.-depth);
     gl_Position=projectionMatrix*modelViewMatrix*vec4(pos,1.);
     
     vUv=uv;
